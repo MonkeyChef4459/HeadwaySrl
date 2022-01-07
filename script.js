904053fdac1fdfec54ad7048e1ed6499a11d6de6
MonkeyChef4459 <93142028+MonkeyChef4459@users.noreply.github.com>
2022-01-07 12:15:38 +0100

    mob

--- script.js
+++ script.js
@@ -3,20 +3,28 @@ function tog(x){
     var body = document.getElementById("body");
     var home = document.getElementById("page1");
     var sub = document.getElementById("sx");
-    var pag = document.getElementById("page1");
+    var schermo = window.screen.width;
     x.classList.toggle("change");
-    if(body.style.overflow === "hidden"){
+    if(body.style.overflow === "hidden" && schermo>1025){
         menu.style.opacity = "0";
         body.style.overflow = "";
         home.style.transform = "translateX(0)";
         sub.style.display = "block";
-        pag.style.opacity = "100%";
-    }else{
+        home.style.opacity = "100%";
+    }else if(body.style.overflow != "hidden" && schermo>1025){
         menu.style.opacity = "100%";
         body.style.overflow = "hidden";
         home.style.transform = "translateX(-30%)";
         sub.style.display = "none";
-        pag.style.opacity = "70%";
+        home.style.opacity = "70%";
+    }else if(body.style.overflow != "hidden" && schermo<1025){
+        home.style.transform = "translateX(-100%)";
+        menu.style.opacity = "100%";
+        body.style.overflow = "hidden";
+    }else if(body.style.overflow === "hidden" && schermo<1025){
+        home.style.transform = "translateX(0)";
+        menu.style.opacity = "0";
+        body.style.overflow = "";
     }
 }
 

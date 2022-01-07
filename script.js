273dbaf45ab98bd78efcb543b7c37021c8514159
MonkeyChef4459 <93142028+MonkeyChef4459@users.noreply.github.com>
2022-01-07 12:29:01 +0100

    mob

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ function tog(x){
     var body = document.getElementById("body");
     var home = document.getElementById("page1");
     var sub = document.getElementById("sx");
+    var page2 = document.getElementById("page2");
+    var page3 = document.getElementById("page3");
+    var page4 = document.getElementById("page4");
+    var page5 = document.getElementById("page5");
     var schermo = window.screen.width;
     x.classList.toggle("change");
     if(body.style.overflow === "hidden" && schermo>1025){
@@ -21,10 +25,18 @@ function tog(x){
         home.style.transform = "translateX(-100%)";
         menu.style.opacity = "100%";
         body.style.overflow = "hidden";
+        page2.style.display = "none";
+        page3.style.display = "none";
+        page4.style.display = "none";
+        page5.style.display = "none";
     }else if(body.style.overflow === "hidden" && schermo<1025){
         home.style.transform = "translateX(0)";
         menu.style.opacity = "0";
         body.style.overflow = "";
+        page2.style.display = "block";
+        page3.style.display = "block";
+        page4.style.display = "block";
+        page5.style.display = "block";
     }
 }
 

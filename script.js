6fb826973e9aa4660a68c6ff50daacddb025cf37
MonkeyChef4459 <93142028+MonkeyChef4459@users.noreply.github.com>
2022-01-21 20:36:22 +0100

    mob

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-function tog(x){
     var menu = document.getElementById("menu");
     var body = document.getElementById("body");
     var home = document.getElementById("page1");
@@ -8,6 +7,12 @@ function tog(x){
     var page4 = document.getElementById("page4");
     var page5 = document.getElementById("page5");
     var schermo = window.screen.width;
+    var uno = document.getElementById("on");
+    var due = document.getElementById("tw");
+    var tre = document.getElementById("tr");
+    var quattro = document.getElementById("fo");
+
+function tog(x){
     x.classList.toggle("change");
     if(body.style.overflow === "hidden" && schermo>1025){
         menu.style.opacity = "0";
@@ -42,6 +47,62 @@ function tog(x){
     }
 }
 
+function open1(){
+    if(uno.style.height != "100%"){
+        page2.style.display = "block";
+        uno.style.height = "100%";
+        uno.style.width = "100%";
+        uno.style.margin = "0";
+        due.style.display = tre.style.display = quattro.style.display = "none";
+        document.getElementById("back").style.display = "block";
+        uno.style.cursor = "auto";
+    }
+}
+
+function open2(){
+    if(due.style.height != "100%"){
+        page2.style.display = "block";
+        due.style.height = "100%";
+        due.style.width = "100%";
+        due.style.margin = "0";
+        uno.style.display = tre.style.display = quattro.style.display = "none";
+        document.getElementById("back").style.display = "block";
+        due.style.cursor = "auto";
+    }
+}
+
+function open3(){
+    if(tre.style.height != "100%"){
+        page2.style.display = "block";
+        tre.style.height = "100%";
+        tre.style.width = "100%";
+        tre.style.margin = "0";
+        uno.style.display = due.style.display = quattro.style.display = "none";
+        document.getElementById("back").style.display = "block";
+        tre.style.cursor = "auto";
+    }
+}
+
+function open4(){
+    if(due.style.height != "100%"){
+        page2.style.display = "block";
+        quattro.style.height = "100%";
+        quattro.style.width = "100%";
+        quattro.style.margin = "0";
+        uno.style.display = tre.style.display = due.style.display = "none";
+        document.getElementById("back").style.display = "block";
+        quattro.style.cursor = "auto";
+    }
+}
+
+function chiudi(){
+    document.getElementById("back").style.display = "none";
+    page2.style.display = "flex";
+    uno.style.display = due.style.display = tre.style.display = quattro.style.display = "block";
+    uno.style.height = due.style.height = tre.style.height = quattro.style.height = "78%";
+    uno.style.margin = due.style.margin = tre.style.margin = quattro.style.margin = "4% 2% auto 2%";
+    uno.style.cursor = due.style.cursor = tre.style.cursor = quattro.style.cursor = "pointer";
+}
 
 
 
